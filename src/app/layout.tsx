caf330d5b2eb3f9d6d30f2812fd0683758809b93
jaoharil <jaoharilmaknun2904@gmail.com>
2025-11-28 21:15:22 +0800

    perbaiki global css

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,10 @@
-import { Poppins } from "next/font/google";
-import "./globals.css";
-import Header from "@/components/Layout/Header";
-import Footer from "@/components/Layout/Footer";
-import { ThemeProvider } from "next-themes";
-import ScrollToTop from "@/components/ScrollToTop";
-const font = Poppins({ subsets: ["latin"], weight: ["400", "500", "600", "700"], });
+import { Poppins } from 'next/font/google';
+import './globals.css';
+import Header from '@/components/Layout/Header';
+import Footer from '@/components/Layout/Footer';
+import { ThemeProvider } from 'next-themes';
+import ScrollToTop from '@/components/ScrollToTop';
+const font = Poppins({ subsets: ['latin'], weight: ['400', '500', '600', '700'] });
 
 export default function RootLayout({
   children,
@@ -14,11 +14,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={`${font.className}`}>
-        <ThemeProvider
-          attribute="class"
-          enableSystem={true}
-          defaultTheme="light"
-        >
+        <ThemeProvider attribute="class" enableSystem={true} defaultTheme="light">
           <Header />
           {children}
           <Footer />
